f0c3335e4cf262f2355f64440188f4c842e83939
lorenzo93d <lorenzo93d@gmail.com>
2016-05-21 17:02:08 +0200

    Add files via upload

---
+++ Esame.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
+
+public class Esame {
+	private String nome;
+	private String codice;
+	private String descrizione;
+	private int costo;
+	private Map<String,String> prerequisiti;
+	private Set<String> tagRisultati; //mappa x risultati?
+//	data\ora,paziente,medico
+	private Paziente paziente;
+	private Date dataPrenotazione;
+	private Date dataEsame;
+	private Medico medico;
+	
+	
+	
+	
+
+}
